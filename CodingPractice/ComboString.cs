5194950161bea755c5ee97b134f31111faf61046
sjd58 <dicksonsj0798@gmail.com>
2023-03-16 20:42:10 -0400

    Adding a coding challenge to keep my C# skills sharp

---
+++ CodingPractice/ComboString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    // Coding Exercise:
+    // Create a string method called ComboString that takes in two strings "a" and "b".
+    // Given 2 strings, a and b, return a string of the form short+long+short, with the
+    // shorter string on the outside and the longer string on the inside. The strings
+    // will not be the same length, but they may be empty (length 0).
+
+    public class MakeComboStrings
+    {
+        public string ComboString(string a, string b)
+        {
+            string longerString = a;
+            string shorterString = b;
+            if (b.Length > a.Length)
+            {
+                longerString = b;
+                shorterString = a;
+            }
+            return shorterString + longerString + shorterString;
+        }
+    }
+}
---
+++ CodingPracticeTests/ComboStringTests.cs
@@ -0,0 +1,46 @@
+﻿using CodingPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPracticeTests
+{
+    [TestClass]
+    public class ComboStringTests
+    {
+        [TestMethod]
+        public void HappyPath()
+        {
+            // Arrange
+            string testStringA = "Hi";
+            string testStringB = "World";
+            MakeComboStrings sut = new MakeComboStrings();
+            string expected = "HiWorldHi";
+
+            // Act
+            string actual = sut.ComboString(testStringA, testStringB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestEmptyStrings()
+        {
+            // Arrange
+
+            string testStringA = "";
+            string testStringB = "Hi";
+            MakeComboStrings sut = new MakeComboStrings();
+            string expected = "Hi";
+
+            // Act
+            string actual = sut.ComboString(testStringA, testStringB);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
