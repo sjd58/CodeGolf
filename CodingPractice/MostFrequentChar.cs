188190e3c5d9c5da50144fb7aabc401f02b5de3d
sjd58 <dicksonsj0798@gmail.com>
2023-03-22 18:36:27 -0400

    Created ReturnMostFrequentChar method

---
+++ CodingPractice/MostFrequentChar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+
+    // Given a string, return the char in the string that comes up most often in the string.
+    public class MostFrequentChar
+    {
+        public char ReturnMostFrequentChar(string input)
+        {
+            List<char> charList = new List<char>();
+            for(int i = 0; i < input.Length; i++)
+            {
+                bool hasChar = false;
+                foreach(char c in charList)
+                {
+                    if (c == input[i])
+                    {
+                        hasChar = true;
+                    }
+                }
+                if (hasChar == false)
+                {
+                    charList.Add(input[i]);
+                }
+            }
+            int[] charCounts = new int[charList.Count];
+            for (int i = 0; i < charList.Count;i++)
+            {
+                int charCount = 0;
+                char c = charList[i];
+                for(int j = 0; j < input.Length; j++)
+                {
+                    if (c == input[j])
+                    {
+                        charCount++;
+                    }
+                }
+                charCounts[i] = charCount;
+            }
+            int greatestCharIndex = 0;
+            int greatestCharCount = 0;
+            for (int i = 0; i < charCounts.Length; i++)
+            {
+                if (charCounts[i] > greatestCharCount)
+                {
+                    greatestCharIndex = i;
+                    greatestCharCount = charCounts[i];
+                }
+            }
+            return charList[greatestCharIndex];
+        }
+
+        /*
+         * TODO: do this method again, but using a dictionary.
+        public char MostFrequentCharUsingDictionary(string input)
+        {
+
+        }
+        */
+    }
+}
---
+++ CodingPracticeTests/MostFrequentCharTests.cs
@@ -0,0 +1,44 @@
+﻿using CodingPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPracticeTests
+{
+    [TestClass]
+    public class MostFrequentCharTests
+    {
+        [TestMethod]
+        public void HappyPath ()
+        {
+            // Arrange
+            MostFrequentChar sut = new MostFrequentChar();
+            string exampleString = "Hello there, hello hello!";
+            char expected = 'l';
+
+            // Act
+            char actual = sut.ReturnMostFrequentChar(exampleString);
+            
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StringLengthOne()
+        {
+            // Arrange
+            MostFrequentChar sut = new MostFrequentChar();
+            string exampleString = "H";
+            char expected = 'H';
+
+            // Act
+            char actual = sut.ReturnMostFrequentChar(exampleString);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
