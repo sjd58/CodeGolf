553268745def950c3aefe00b3e87692e74f89174
sjd58 <dicksonsj0798@gmail.com>
2023-04-27 17:34:22 -0400

    saving some new coding exercises I worked on

---
+++ CodingPractice/EvenlySpaced.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    public class EvenlySpaced
+    {
+        // Create a boolean method called EvenlySpaced that takes in
+        // three integers, "a", "b", and "c"; given three ints, a, b, and c,
+        // one of them is small, one is medium, and one is large. Return 
+        // true if the three values are evenly spaced, so the difference
+        // between small and medium is the same as the difference between 
+        // the medium and large ints.
+        // nums = [6, 4, 2]
+        // nums = [4, 6, 2]
+        // nums = [4, 2, 6]
+        public bool IntsEvenlySpaced(int[] nums)
+        {
+            int placeholder = 0;
+            int small = nums[0];
+            int medium = nums[1];
+            int large = nums[2];
+            if (small > medium)
+            {
+                placeholder = small;
+                small = medium;
+                medium = placeholder;
+            }
+            if (medium > large)
+            {
+                placeholder = medium;
+                medium = large;
+                large = placeholder;
+            }
+            if (small > large)
+            {
+                placeholder = large;
+                large = small;
+                small = placeholder;
+            }
+            if (small > medium)
+            {
+                placeholder = small;
+                small = medium;
+                medium = placeholder;
+
+            }
+
+            int smallMediumSpace = medium - small;
+            int mediumLargeSpace = large - medium;
+
+            if (smallMediumSpace == mediumLargeSpace)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CodingPracticeTests/EvenlySpacedTests.cs
@@ -0,0 +1,70 @@
+﻿using CodingPractice;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPracticeTests
+{
+    [TestClass]
+    public class EvenlySpacedTests
+    {
+        [TestMethod]
+        public void HappyPath()
+        {
+            // Arrange
+            int[] testArray = new int[3] { 2, 4, 6 };
+            EvenlySpaced sut = new EvenlySpaced();
+
+            // Act
+            bool actual = sut.IntsEvenlySpaced(testArray);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReverseOrder()
+        {
+            // Arrange
+            int[] testArray = new int[3] { 6, 4, 2 };
+            EvenlySpaced sut = new EvenlySpaced();
+
+            // Act
+            bool actual = sut.IntsEvenlySpaced(testArray);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void MediumLargerThanLarge()
+        {
+            // Arrange
+            int[] testArray = new int[3] { 2, 6, 4 };
+            EvenlySpaced sut = new EvenlySpaced();
+
+            // Act
+            bool actual = sut.IntsEvenlySpaced(testArray);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void CheckReturnsFalse()
+        {
+            // Arrange
+            int[] testArray = new int[3] { 6, 2, 15 };
+            EvenlySpaced sut = new EvenlySpaced();
+
+            // Act
+            bool actual = sut.IntsEvenlySpaced(testArray);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}
