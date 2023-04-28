0fb97ac6d7ee0354944e3bef49eb49600d5df638
sjd58 <dicksonsj0798@gmail.com>
2023-04-28 16:56:05 -0400

    saving some coding practice problems

---
+++ CodingPractice/PrimeFactors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    public class PrimeFactors
+    {
+        public int[] ReturnPrimeFactors(int num)
+        {
+            List<int> primeFactorsList = new List<int>();
+
+            for (int i = 2; i <= num; i++)
+            {
+                while (num % i == 0)
+                {
+                    primeFactorsList.Add(i);
+                    num /= i;
+                }
+            }
+
+            int[] arrayToReturn = new int[primeFactorsList.Count];
+            for (int i = 0; i < primeFactorsList.Count; i++)
+            {
+                arrayToReturn[i] = primeFactorsList[i];
+            }
+            return arrayToReturn;
+        }
+    }
+}
---
+++ CodingPracticeTests/PrimeFactorsTests.cs
@@ -0,0 +1,58 @@
+﻿using CodingPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPracticeTests
+{
+    [TestClass]
+    public class PrimeFactorsTests
+    {
+        [TestMethod]
+        public void HappyPath()
+        {
+            // Arrange
+            int testNumber = 12;
+            PrimeFactors sut = new PrimeFactors();
+            int[] expected = new int[3] { 2, 2, 3 };
+
+            // Act
+            int[] actual = sut.ReturnPrimeFactors(testNumber);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeNumber()
+        {
+            // Arrange
+            int testNumber = 13;
+            PrimeFactors sut = new PrimeFactors();
+            int[] expected = new int[1] { 13 };
+
+            // Act
+            int[] actual = sut.ReturnPrimeFactors(testNumber);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ManyManyPrimes()
+        {
+            // Arrange
+            int testNumber = 144;
+            PrimeFactors sut = new PrimeFactors();
+            int[] expected = new int[6] { 2, 2, 2, 2, 3, 3 };
+
+            // Act
+            int[] actual = sut.ReturnPrimeFactors(testNumber);
+
+            // Assert
+            CollectionAssert.AreEqual (expected, actual);
+        }
+    }
+}
